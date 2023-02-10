dd9911dfe7b1ebd594c74c3b1ee2c5fd389b9425
Ahnm1 <hansson.alexander@live.se>
2023-02-10 16:44:30 +0100

    Graph classes

---
+++ LineGraph.py
@@ -0,0 +1,26 @@
+import plotly.express as px
+
+class LineGraph:
+    def __init__(self):
+        pass
+    
+    def get_prices_graph(self):
+        pass
+
+    def get_multi_energy_graph(self, dfe_no_total):
+        fig_gwha = px.line(dfe_no_total, x='month', y='amount', color='type')
+        fig_gwha.update_layout(
+            {'legend': {'title': 'Type'},
+            'xaxis': {'title': {'text': 'Date'}},
+            'yaxis': {'title': {'text': 'GWh'}}}
+        )
+        return fig_gwha
+    
+    def get_line_energy_graph(self, df_total):
+        fig_gwht = px.line(df_total.where(df_total['type'] == 'Total'), x = 'month', y = 'amount')
+        fig_gwht.update_layout(
+            {'legend': {'title': 'Type'},
+            'xaxis': {'title': {'text': 'Date'}},
+            'yaxis': {'title': {'text': 'GWh'}}}
+        )
+        return fig_gwht
---
+++ Scatter3D.py
@@ -0,0 +1,28 @@
+import plotly.express as px
+
+class Scatter3D:
+    def __init__(self):
+        pass
+
+    def get_plot(self, df_price):
+        fig_3d = px.scatter_3d(
+            df_price,
+            x = 'Lufttemperatur AVG',
+            y = 'Vindhastighet AVG',
+            z = 'SpotPriceEUR',
+            color = 'Vindhastighet AVG'
+        )
+        fig_3d.update_layout(
+            {'coloraxis': {'colorbar': {'title': {'text': 'Wind Average'}}}}
+        )
+        fig_3d.update_scenes( 
+            {
+                'xaxis': {'title': {'text': 'Air Temperature'}},
+                'yaxis': {'title': {'text': 'Wind Average'}},
+                'zaxis': {'title': {'text': 'Spot Price €'}}
+            },
+            xaxis_autorange="reversed"
+        )
+        return fig_3d
+
+
