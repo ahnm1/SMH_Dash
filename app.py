f3b9a1557391903983733d6602f33899c05a2dff
Ahnm1 <hansson.alexander@live.se>
2023-02-13 21:42:45 +0100

    New model implemented

--- app.py
+++ app.py
@@ -56,27 +56,34 @@ app.layout = html.Div(children=[
             html.Div([
                 
                 html.Div([
-                    "Hour: ",
+                    'Hour: ',
                     dcc.Input(id='hour-in', value='0', type='number')
                 ]),
                 html.Br(),
                 html.Div([
-                    "Month: ",
-                    dcc.Input(id='month-in', value='0', max = 12, type='number')
+                    'Day: ',
+                    dcc.Input(id='day-in', value='1', max = 7,min = 1, type='number')
                 ]),
                 html.Br(),
                 html.Div([
-                    "Wind: ",
-                    dcc.Input(id='wind-in', value='0', type='number')
+                    'Month: ',
+                    dcc.Input(id='month-in', value='1', max = 12, min = 1, type='number')
                 ]),
                 html.Br(),
                 html.Div([
-                    "Temperature: ",
+                    'Wind: ',
+                    dcc.Input(id='wind-in', value='0', min = 0, type='number')
+                ]),
+                html.Br(),
+                html.Div([
+                    'Temperature: ',
                     dcc.Input(id='temp-in', value='0', type='number')
                 ]),
                 ], id = 'augur-inner'),
                 html.Br(),
                 html.Div(id='hour-output'),
+                html.Div(id='day-output'),
+                
                 html.Div(id='month-output'),
                 html.Div(id='wind-output'),
 
@@ -103,26 +110,29 @@ app.layout = html.Div(children=[
             id = 'multi-graph-gwh',
             figure = fig_gwha
         ),
+    
 ], id = 'html-div')
 
 @app.callback(
     Output(component_id='hour-output', component_property='children'),
+    Output(component_id='day-output', component_property='children'),
     Output(component_id='month-output', component_property='children'),
     Output(component_id='wind-output', component_property='children'),
     Output(component_id='temp-output', component_property='children'),
     Output(component_id='result-output', component_property='children'),
     Input(component_id='hour-in', component_property='value'),
+    Input(component_id='day-in', component_property='value'),
     Input(component_id='month-in', component_property='value'),
     Input(component_id='wind-in', component_property='value'),
     Input(component_id='temp-in', component_property='value'),
     Input(component_id='fake-in', component_property='value'),
 )
-def update_output_div(hour_val, month_val, wind_val, temp_val, result_str):
+def update_output_div(hour_val, day_val, month_val, wind_val, temp_val, result_str):
     if month_val == '0':
         print(f'{month_val}: no go', type(month_val))
         return (
-            f'At {hour_val}:00,',
-            f'in th month of 0',
+            f'A {day_val}', f'at {hour_val}:00,',
+            f'in the month of 0',
             f'with wind average at {wind_val} m/s',
             f'and average temperatur of {temp_val}°C',
             f'Predicted price is: 0')
@@ -130,19 +140,19 @@ def update_output_div(hour_val, month_val, wind_val, temp_val, result_str):
     elif month_val == None:
         print(f'{month_val}: no go', type(month_val))
         return (
-            f'At {hour_val}:00,',
-            f'in th month of 0',
+            f'A {day_val}', f'at {hour_val}:00,',
+            f'in the month of 0',
             f'with wind average at {wind_val} m/s',
             f'and average temperatur of {temp_val}°C',
             f'Predicted price is: 0')
     else:
         print(f'{month_val}: yes go', type(month_val))
-        result_str = ml.get_input_and_predict(wind_val, temp_val, month_val, hour_val)
+        result_str = ml.get_input_and_predict(wind_val, temp_val, month_val, hour_val, day_val)
         # print(a)
 
         return (
-            f'At {hour_val}:00,',
-            f'in th month of {calendar.month_name[int(month_val)]}',
+            f'A {calendar.day_name[int(day_val)]}', f'at {hour_val}:00,',
+            f'in the month of {calendar.month_name[int(month_val)]}',
             f'with wind average at {wind_val} m/s',
             f'and average temperatur of {temp_val}°C',
             f'Predicted price is: {result_str}, SEK / KWh')
