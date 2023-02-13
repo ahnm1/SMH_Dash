265a090ed409faef1038c84f9d8b66ed48e0559b
Ahnm1 <hansson.alexander@live.se>
2023-02-13 08:14:21 +0100

    Predict widget implemented

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 #%%
-from dash import Dash, html, dcc
+import calendar
+from dash import Dash, html, dcc, Input, Output
 import plotly.express as px
 import plotly.graph_objects as go
 import pandas as pd
@@ -7,6 +8,7 @@ import plotly.io as pio
 
 from Scatter3D import Scatter3D
 from LineGraph import LineGraph
+from ML.MLPredict import MLPredict
 
 pio.templates.default = 'plotly_dark' # 'ggplot2' 
 #%%
@@ -33,32 +35,117 @@ fig_gwha = LineGraph().get_multi_energy_graph(dfe_no_total)
 fig_gwht = LineGraph().get_line_energy_graph(dfe.where(dfe['type'] == 'Total'))
 
 fig_temp_wind = px.line(df_price, x = 'Timestamp', y = ['Vindhastighet AVG', 'Lufttemperatur AVG'])
-
+ml  = MLPredict()
 app = Dash(__name__)
 app.layout = html.Div(children=[
-    html.H1(children='W i n d + T e m p e r a t u r e'),
+    # html.H1(children='Wind + Temperature ?'),
     dcc.Graph(
             id='s3d-graph',
-            figure=fig_3d
+            figure = fig_3d
         ),
-    html.H1(children = 'D o e s     i t     =   P r i c e ?'),
+    # html.H1(children = 'Does It = Price?'),
+    # html.P(children = 'So is there a correlation \
+    #     between temperature, wind and price for electricity.'),
     html.Div(children = [
         dcc.Graph(
-            id='line-graph-price',
-            figure=fig_temp_wind,
+            id='line-graph-qwht',
+            figure = fig_gwht,
         ),
-        dcc.Graph(
-            id='line-graph-gwht',
-            figure=fig_gwht,
-        ),   
+        html.Div([
+            html.H3('Predict Price', style=({'align-items': 'center'})),
+            html.Div([
+                
+                html.Div([
+                    "Hour: ",
+                    dcc.Input(id='hour-in', value='0', type='number')
+                ]),
+                html.Br(),
+                html.Div([
+                    "Month: ",
+                    dcc.Input(id='month-in', value='0', max = 12, type='number')
+                ]),
+                html.Br(),
+                html.Div([
+                    "Wind: ",
+                    dcc.Input(id='wind-in', value='0', type='number')
+                ]),
+                html.Br(),
+                html.Div([
+                    "Temperature: ",
+                    dcc.Input(id='temp-in', value='0', type='number')
+                ]),
+                ], id = 'augur-inner'),
+                html.Br(),
+                html.Div(id='hour-output'),
+                html.Div(id='month-output'),
+                html.Div(id='wind-output'),
+
+                html.Div(id='temp-output'),
+                # html.P('The predicted price is:'),
+
+                html.Div(id='result-output',style=({'font-size': 22})),
+                html.P(id='fake-in'),
+            
+        ],id= 'augur-outer',style=({
+            'align-items': 'center',
+            'margin-left': 42,
+            'padding-left': 16,
+            'margin-right': 42,
+            'padding-right': 16,
+            }
+            ))
     ], id = 'graph-div'),
-    html.P(children = 'So is there a correlation \
-        between temperature, wind and price for electricity.'),
+        dcc.Graph(
+            id = 'line-graph-price',
+            figure = fig_temp_wind,
+        ),  
     dcc.Graph(
-            id='multi-graph-gwh',
-            figure=fig_gwha
+            id = 'multi-graph-gwh',
+            figure = fig_gwha
         ),
 ], id = 'html-div')
 
+@app.callback(
+    Output(component_id='hour-output', component_property='children'),
+    Output(component_id='month-output', component_property='children'),
+    Output(component_id='wind-output', component_property='children'),
+    Output(component_id='temp-output', component_property='children'),
+    Output(component_id='result-output', component_property='children'),
+    Input(component_id='hour-in', component_property='value'),
+    Input(component_id='month-in', component_property='value'),
+    Input(component_id='wind-in', component_property='value'),
+    Input(component_id='temp-in', component_property='value'),
+    Input(component_id='fake-in', component_property='value'),
+)
+def update_output_div(hour_val, month_val, wind_val, temp_val, result_str):
+    if month_val == '0':
+        print(f'{month_val}: no go', type(month_val))
+        return (
+            f'At {hour_val}:00,',
+            f'in th month of 0',
+            f'with wind average at {wind_val} m/s',
+            f'and average temperatur of {temp_val}°C',
+            f'Predicted price is: 0')
+
+    elif month_val == None:
+        print(f'{month_val}: no go', type(month_val))
+        return (
+            f'At {hour_val}:00,',
+            f'in th month of 0',
+            f'with wind average at {wind_val} m/s',
+            f'and average temperatur of {temp_val}°C',
+            f'Predicted price is: 0')
+    else:
+        print(f'{month_val}: yes go', type(month_val))
+        result_str = ml.get_input_and_predict(wind_val, temp_val, month_val, hour_val)
+        # print(a)
+
+        return (
+            f'At {hour_val}:00,',
+            f'in th month of {calendar.month_name[int(month_val)]}',
+            f'with wind average at {wind_val} m/s',
+            f'and average temperatur of {temp_val}°C',
+            f'Predicted price is: {result_str}, SEK / KWh')
+
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run_server(debug = True)
